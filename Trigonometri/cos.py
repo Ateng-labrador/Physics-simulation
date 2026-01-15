aa1d25f0c0c22e375bfb93a09985a47e7a1027e2
Arlan Tengga <arlantengga8@gmail.com>
2026-01-15 18:04:01 +0700

    fix: memperbaiki bug

--- Trigonometri/cos.py
+++ Trigonometri/cos.py
@@ -3,23 +3,27 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-def parameter(t = np.linspace(0, 10, 100)):
-    y = np.sin(t)
+def parameter(t=np.linspace(0, 10, 100)):
+    """Fungsi untuk Parameter"""
+    y = np.cos(t)
     return y, t
 
 
 def setup_axis(t, axis):
+    """Fungsi untuk setup axis"""
     axis.set_xlim([min(t), max(t)])
     axis.set_ylim([-2, 2])
     return axis
 
 
 def setup_plot(axis):
-    (animation_plot) = axis.plot([], [])
+    """Fungsi untuk setup plot"""
+    (animation_plot,) = axis.plot([], [])
     return animation_plot
 
 
 def animation(i, t, y, animation_plot):
+    """Fungsi untuk membuat frame to frame"""
     animation_plot.set_data(t[:i], y[:i])
     return animation_plot
 
@@ -30,18 +34,18 @@ if __name__ == "__main__":
     axis = setup_axis(t, axis)
     animation_plot = setup_plot(axis)
 
-
     def init():
+        """Animasi awal"""
         animation_plot.set_data([], [])
-        return [animation_plot]
+        return (animation_plot,)
 
-    ani = FuncAnimation(
+    _ani = FuncAnimation(
         fig=fig,
         func=animation,
         fargs=(t, y, animation_plot),
         init_func=init,
         frames=len(t),
         interval=30,
-        blit=False
+        blit=False,
     )
     plt.show()
